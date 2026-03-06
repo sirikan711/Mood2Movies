89211d5d452caea1bab05d991c4019db6fde51dc
sirikan711 <sirikan.ao.66@ubu.ac.th>
2026-03-06 21:27:06 +0700

    final

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,4 @@
+# movies/admin.py
 from django.contrib import admin
 from django.utils.html import format_html
 from .models import Movie, Mood, Review, Favorite, Bookmark, CustomList, Profile, ReviewMoodScore
